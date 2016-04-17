f6cae920334799d9ef0ae2b4e518a3147309c619
Lin <caiqinghua@126.com>
2016-04-17 16:54:22 +0800

    add Distributor Quantity Purchase to Debit::PAYMENT_MODE

--- app/models/spree/debit.rb
+++ app/models/spree/debit.rb
@@ -1,7 +1,7 @@
 module Spree
   class Debit < StoreCredit
     # Negative Payment Mode cannot be set manually. They are reserved for some particular task internally.
-    PAYMENT_MODE = { 'Order Purchase' => -1, 'Deduce' => 0 }
+    PAYMENT_MODE = { 'Distributor Quantity Purchase' => -2, 'Order Purchase' => -1, 'Deduce' => 0  }
 
     include Spree::DisableNegativePaymentModeAndSetBalanceAbility
 
